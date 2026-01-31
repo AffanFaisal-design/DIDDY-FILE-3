6904bab95c818117542d60f710328e6c83f9c91e
Affan Hassaan <affan.hassaan@gmail.com>
2026-01-31 17:43:41 +0500

    Add requirements and fix file paths

--- app.py
+++ app.py
@@ -462,6 +462,5 @@ def export_pdf():
 # =========================================
 # RUN APP
 # =========================================
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+if _name_ == "_main_":
+    app.run()
